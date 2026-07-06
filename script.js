03c0aea275d18c5449057fd1e00806e15b19f2a6
dikeshu9-jpg <dike.shu9@gmail.com>
2026-07-07 01:17:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const today = new Date();
 
+const text =
+today.getFullYear() + "年" +
+(today.getMonth()+1) + "月" +
+today.getDate() + "日";
+
+const el = document.getElementById("today");
+
+if(el){
+    el.textContent = text;
+}
